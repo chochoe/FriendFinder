d011307fbe1f64a8c82e5a4d0e8a4d5299a14102
Emery Cho <cho.e@live.com>
2017-11-16 23:17:30 -0800

    functionality done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,31 @@
-app.get("/api/friends", function(req, res) {
-  
-});
-
-app.post("/api/friends", function(req, res) {
-
-});
+module.exports = function (app) {
+	var path = require('path');
+	var friends = require('../data/friends.js');
+	app.get('/api/friends', function (req, res) {
+		res.json(friends);
+	});
+	app.post('/api/friends', function (req, res) {
+		var totalDifference = [];
+    	for (var i = 0; i < friends.length; i++){
+    		var match = friends[i].scores;
+    		for (var j = 0; j < match.length; j++) {
+    			var difference = 0;
+    			difference += Math.abs(req.body.scores[j] - match[j]);
+    		}
+        	totalDifference.push(difference);
+        };
+        var index = 0;
+		var value = totalDifference[0];
+		function lowestIndex (arr) {
+			for (var i = 1; i < arr.length; i++) {
+	  			if (arr[i] < value) {
+	    			value = arr[i];
+	    			index = i;
+	  			}
+			};
+			return index;
+		};
+		var myMatch = friends[lowestIndex(totalDifference)];
+		res.send(myMatch);
+	});
+};
--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-var express = requires("express");
-var bodyParser = requires("body-parser");
-var path = requires("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+var app = express();
+var PORT = 3309;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+htmlRoutes(app);
+apiRoutes(app);
+
+app.use(express.static('app/public'));
+
+app.listen(PORT, function() {
+	console.log("Server listening on port " + PORT + ".");
+});
